83e219fc459c7999440d53d28824e49b7cc1b079
anhtudo97 <anhtudo97@gmail.com>
2022-10-03 00:37:08 +0700

    feat: add package

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      App Hoo Bank
-    </div>
-  )
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
-export default App
+export default App;
